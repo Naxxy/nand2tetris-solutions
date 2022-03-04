37161bb98a1233493f0c08fa0c27bc3c7a2430c6
Naxxy <alex.gugz@gmail.com>
2022-03-04 14:36:37 +0800

    Add method stubs for CompilationEngine interface

--- projects/10/CompilationEngine.py
+++ projects/10/CompilationEngine.py
@@ -1,7 +1,68 @@
 #!/usr/bin/env python
 
 class CompilationEngine:
-    pass
+    fp = None
+
+    def __init__(self, filepath: str):
+        self.fp = open()
+
+    def compileClass(self):
+        pass
+
+    def compileClassVarDec(self):
+        pass
+
+    def compileSubroutine(self):
+        pass
+
+    def compileParameterList(self):
+        pass
+
+    def compileSubroutineBody(self):
+        pass
+
+    def compileVarDec(self):
+        """
+        Example:
+            <varDec>
+              <keyword> var </keyword>
+              <identifier> Array </identifier>
+              <identifier> a </identifier>
+              <symbol> ; </symbol>
+            </varDec>
+        """
+        pass
+
+
+    def compileStatements(self):
+        pass
+
+    def compileLet(self):
+        pass
+
+    def compileIf(self):
+        pass
+
+    def compileWhile(self):
+        pass
+
+    def compileDo(self):
+        pass
+
+    def compileReturn(self):
+        pass
+
+    def compileExpression(self):
+        pass
+
+    def compileTerm(self):
+        pass
+
+    def compileExpressionList(self):
+        pass
+
+
+
 
 if __name__ == "__main__":
     print("Inside CompilationEngine")
